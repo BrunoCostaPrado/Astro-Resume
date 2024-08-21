bcb168d8c4a5338c3c49d1920c34516a85c38990
Bruno Costa Prado <brunocostaprado@gmail.com>
2024-08-21 04:03:51 -0300

    add analitc

---
+++ public/scripts/analytics.ts
@@ -0,0 +1,9 @@
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-141JZH3SYZ"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-141JZH3SYZ');
+// </script>
