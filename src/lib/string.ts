258e63ab04dffcd505c19897b909ae873a79c84b
Bruno Costa Prado <brunocostaprado@gmail.com>
2024-08-28 23:16:01 -0300

    edit

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,9 +1,11 @@
 export const documentation: string =
   "Learn how Astro works and explore the official API docs."
 
-export const integrations: string = ""
+export const integrations: string =
+  "Supercharge your project with new frameworks and libraries."
 
-export const themes: string = ""
+export const themes: string =
+  "Explore a galaxy of community-built starter themes."
 
 export const community: string =
   "Come say hi to our amazing Discord community. ❤️"
